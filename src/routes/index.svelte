cac1aa8cf6b6e854aa8b45b739b50d28b6d26660
Арсений Сергеевич Белоусов <s0153348@edu.kubsu.ru>
2022-07-16 12:28:41 +0300

    third steps

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,12 @@
-<script>
-import Header from "./header.svelte";
-import Head from "./head.svelte";
-import Nav from "./nav.svelte"
-import ZhKCard from "./zh_k_card.svelte";
-let fetchApartments = async () => {
-    const url = "https://"
-}
+<script lang="ts">
+	import Header from './header.svelte';
+	import Head from './head.svelte';
+	import Nav from './nav.svelte';
+	import ZhKCard from './zh_k_card.svelte';
+ //   import {apartmentCards} from '../stores/apartmentsStore';
+ //   import {data_type} from '../stores/apartmentsStore';
 </script>
-<Head/>
-<Header/>
-<Nav/>
-<ZhKCard img_src={"fffff"} name={"ЖК Фонтаны"} descr={"Ваще клевый жк, мамой клянусь"}/> 
+
+<Head />
+<Header />
+<Nav />
--- src/routes/zh_k_card.svelte
+++ src/routes/zh_k_card.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     export let img_src: string;
-    export let name = "";
-    export let descr = "";
+    export let name: string;
+    export let builder: string;
+    export let descr: string;
 </script>
 <div class="col-md-12 col-lg-4 services-2 p-4 py-5 d-flex ftco-animate fadeInUp ftco-animated">
     <div class="py-3 d-flex">
@@ -10,6 +11,7 @@
         </div>
         <div class="text">
           <h3>{name}</h3>
+          <h2>{builder}</h2>
           <p>{descr}</p>
         </div>
     </div>
